e3704f8efcfc3cbce4e3549dcc2739d6c9df4187
misoronfox <arturoedo@gmail.com>
2022-01-28 21:09:14 -0300

    Revert "Consumiendo api"
    This reverts commit 6b819c12a7262e92d5c811b5f2a94e9146697d3e.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,36 +1,71 @@
 import ItemCount from './ItemCount';
-import {Card, ListGroup, Button } from "react-bootstrap";
-import ItemDetailContainer from './ItemDetailContainer';
+import {Card, ListGroup } from "react-bootstrap";
+
+/*const Item = ({items}) => {
+
+    let listItem = prop.map(product => {    
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        {product.id}
+                    </div>
+                    <div className='col'>
+                        {product.title}
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        {product.pictureUrl}
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        {product.price}
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <ItemCount/>
+                    </div>
+                </div>
+            </div>
+            return(listItem)
+    })*/
+
+
+    /*let listItem = items.map((item) =>
+        <div>
+            {item}
+        </div>
+            
+    );
+        return listItem;
+          
+
+}*/
+
 
 
 const Item = ({ product }) => {
-    const {title, price, category, description, image} = product
-    
-
-    const showDetails = (product) => {
-        
-        return(<ItemDetailContainer product = {product} />)
-    }
-
+    const {title, price, pictureUrl} = product
     return (
         <Card >
-            <Card.Img variant="top" src={image}  />
+            <Card.Img variant="top" src={pictureUrl} />
             <Card.Body >
                 <Card.Text >
                     {title}
                 </Card.Text>
                 <Card.Title>
-                    ${price}
+                    {price}
                 </Card.Title>          
             </Card.Body>              
             <Card.Header>Detalles</Card.Header>            
             <ListGroup className="list-group-flush">
                 <ListGroup.Item>
-                {description}
+                
                 </ListGroup.Item>    
             </ListGroup>             
             <Card.Body>
-                <Button onClick={() => showDetails(product)} className='btn btn-outline-primary' href='./ItemDetailContainer'>Mostrar más</Button>
                 <ItemCount/>
             </Card.Body>                
         </Card> 
--- src/services/Products.js
+++
@@ -1,4 +0,0 @@
-export const getProductsFromCategory = (category) => {
-    return fetch(`https://fakestoreapi.com/products/category/${category}`)
-        .then(data => data.json())
-}
